38a86a4ab70dbfa692721835218de8c81e933e09
BohdanDeveloper <radchukyou1999@gmail.com>
2022-05-22 20:11:30 +0300

    deleted style from notification

--- src/components/Feedback/Components/Notification/Notification.jsx
+++ src/components/Feedback/Components/Notification/Notification.jsx
@@ -1,6 +1,6 @@
-import styles from "./Notification.module.scss";
 
-const Notification = ({message})=>{
+
+const Notification = ({ message }) => {
     return <h2>{message}</h2>
 }
 export default Notification;
